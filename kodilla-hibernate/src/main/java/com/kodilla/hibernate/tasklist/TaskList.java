1decd9b778e30f820c2c472b20210c8b82f72491
IngaPanczyk <ingawaaa@gmail.com>
2020-09-23 22:04:26 +0200

    ćwiczenia z realacjami 1:N oraz M:N

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -5,6 +5,7 @@ import com.kodilla.hibernate.task.Task;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 //@Table(name = "TASKLIST")
@@ -12,11 +13,12 @@ public final class TaskList {
     private int id;
     private String listName;
     private String description;
+    private List<Task> tasks = new ArrayList<>();
 
     public TaskList() {
     }
 
-    public TaskList( String listName, String description) {
+    public TaskList(String listName, String description) {
         this.id = id;
         this.listName = listName;
         this.description = description;
@@ -38,6 +40,12 @@ public final class TaskList {
         return description;
     }
 
+    @OneToMany(targetEntity = Task.class, mappedBy = "taskList", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
@@ -48,5 +56,9 @@ public final class TaskList {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/task/dao/TaskDaoTestSuite.java
@@ -72,4 +72,5 @@ public class TaskDaoTestSuite {
         //CleanUp
         //taskDao.deleteById(id);
     }
+
 }
